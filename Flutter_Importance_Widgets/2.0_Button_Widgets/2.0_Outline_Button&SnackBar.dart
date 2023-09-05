1ce621b66e8a9d5b6c55eb8e94f6c53b2575c60f
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-09-05 23:15:27 +0600

    2.0_Outline_Button&SnackBar.dart

---
+++ Flutter_Importance_Widgets/2.0_Button_Widgets/2.0_Outline_Button&SnackBar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Flutter Button'),
+              centerTitle: true,
+              titleTextStyle: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+            body: Center(
+              child: SizedBox(
+                height: 70,
+                width: 200,
+                child: OutlinedButton(
+                  child: Text(
+                    'Outline Button',
+                  ),
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('Outline button Clicked!!'),
+                        action: SnackBarAction(
+                          label: 'undo',
+                          onPressed: () {},
+                        )));
+                  },
+                  style: OutlinedButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.deepOrangeAccent,
+                  ),
+                ),
+              ),
+            )));
+  }
+}
+
