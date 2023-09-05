b658a9bfcde97fd3f7c00ea0bbe2e6e6b162cb86
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-09-05 22:53:45 +0600

    1.0_Elevated_Button.dart

--- Flutter_Importance_Widgets/2.0_Button_Widgets/1.0_Elevated_Button.dart
+++ Flutter_Importance_Widgets/2.0_Button_Widgets/1.0_Elevated_Button.dart
@@ -1,1 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Flutter Button'),
+              centerTitle: true,
+              titleTextStyle: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ElevatedButton(
+                    onPressed: null,
+                    child: Text('Disable'),
+                  ),
+                  SizedBox(height: 15),
+                  ElevatedButton(onPressed: () {}, child: Text('Enabled')),
+                  SizedBox(height: 15),
+                  ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.message),
+                      label: Text('Enabled'))
+                ],
+              ),
+            )));
+  }
+}
 
