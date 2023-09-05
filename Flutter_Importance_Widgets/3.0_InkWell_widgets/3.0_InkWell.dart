a32573aa73dc67bf2ec93d437dd459cd5688d094
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-09-05 23:41:35 +0600

    3.0_InkWell.dart

--- Flutter_Importance_Widgets/3.0_InkWell_widgets/3.0_InkWell.dart
+++ Flutter_Importance_Widgets/3.0_InkWell_widgets/3.0_InkWell.dart
@@ -1,1 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Flutter Button'),
+              centerTitle: true,
+              titleTextStyle: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+            body: Center(
+              child: InkWell(
+                onTap: () {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('on Tap clicked!!'),
+                    action: SnackBarAction(
+                      label: 'undo',
+                      onPressed: () {},
+                    ),
+                  ));
+                },
+                onDoubleTap: () {    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('Double Taped!!'),
+                  action: SnackBarAction(
+                    label: 'undo',
+                    onPressed: () {},
+                  ),
+                ));},
+                onLongPress: () {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('on LongPressed!!'),
+                    action: SnackBarAction(
+                      label: 'undo',
+                      onPressed: () {},
+                    ),
+                  ));
+                },
+                child: Container(
+                  child: Center(child: Text("Tap Here!",style: TextStyle(
+                    fontSize: 30,fontWeight: FontWeight.bold,
+                  ),)),
+                  height: 200,
+                  width: 300,
+                  color: Colors.deepOrangeAccent,
+                ),
+              ),
+            )));
+  }
+}
 
