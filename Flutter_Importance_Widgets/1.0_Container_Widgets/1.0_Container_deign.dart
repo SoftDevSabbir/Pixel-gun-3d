b57f1f8998b3c94bfa7c784f10cf9e8d8bc0b281
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-09-05 22:04:02 +0600

    1.0_Container_deign.dart

--- Flutter_Importance_Widgets/1.0_Container_Widgets/1.0_Container_deign.dart
+++ Flutter_Importance_Widgets/1.0_Container_Widgets/1.0_Container_deign.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Container'),
+        centerTitle: true,
+        titleTextStyle: TextStyle(color: Colors.orange,fontSize: 35,fontWeight: FontWeight.bold),
+      ),
+
+
+
+         body: Center(
+           child: Container(
+             height: 300,
+             width: 300,
+             color: Colors.orange,
+             child:Center(child: Text("This is container",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),))
+
+           ),
+         )
+    )
+    );
+  }
+}
 
