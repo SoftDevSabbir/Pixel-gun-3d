0bf7a181a3c385366f3147192b437270a2f109b2
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-09-07 06:37:16 +0600

    1.0_ListView.dart

--- Flutter_Importance_Widgets/6.0_ListView/1.0_ListView.dart
+++ Flutter_Importance_Widgets/6.0_ListView/1.0_ListView.dart
@@ -1,1 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var name=['Sabbir','Abir','Akash','Likhon','RAhul','Danial','Jibon','Rohan','Atik'];
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Container Decoration'),
+              centerTitle: true,
+              titleTextStyle: TextStyle(
+                  color: Colors.orange,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+            body:Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      color: Colors.amber,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      color: Colors.cyan,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      color: Colors.pink,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      color: Colors.brown,
+                    ),
+                  ),
+                ],
+              ),
+            )
+            )
+
+        );
+  }
+}
 
