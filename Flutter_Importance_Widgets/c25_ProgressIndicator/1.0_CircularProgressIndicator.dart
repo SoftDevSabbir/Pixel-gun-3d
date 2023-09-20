4cc1b79802b702ac7f5ddd84810b254ad5ea1ee4
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-09-20 22:07:17 +0600

    1.0_CircularProgressIndicator.dart

---
+++ Flutter_Importance_Widgets/c25_ProgressIndicator/1.0_CircularProgressIndicator.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home:MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title:Text("Flutter Widgets"),
+    ),
+    body: Center(child: CircularProgressIndicator(
+      backgroundColor: Colors.green,
+      color: Colors.deepOrange,
+      strokeWidth: 5,
+    )),
+  );
+  }
+  //const MyHomePage({super.key});
+
+}
