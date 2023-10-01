115f1774997ba598bb27f62019aac5c02a5a6330
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-10-01 16:26:23 +0600

    1.0_Custom_AppColors.dart

---
+++ Flutter_Importance_Widgets/c27_Custom_Nessery_Material/1.0_Custom_AppColors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const baseDarkPinkColor = Color(0xff6930c3);
+  static const baseLightPinkColor = Color(0xff9775fa);
+  static const baseDarkGreenColor = Color(0xff23bd66);
+  static const baseLightGreenColor = Color(0xff5dd136);
+  static const baseDarkOrangeColor = Color(0xffff671c);
+  static const baseLightOrangeColor = Color(0xffffa601);
+  static const baseLightCyanColor = Color(0xff05c2bd);
+  static const baseBlackColor = Color(0xff1b1b1d);
+  static const baseGrey90Color = Color(0xff2d2d2f);
+  static const baseGrey80Color = Color(0xff3c3c3e);
+  static const baseGrey70Color = Color(0xff545455);
+  static const baseGrey60Color = Color(0xff727273);
+  static const baseGrey50Color = Color(0xff8d8d8e);
+  static const baseGrey40Color = Color(0xffbababa);
+  static const baseGrey30Color = Color(0xffd0d0d0);
+  static const baseGrey20Color = Color(0xffe6e6e7);
+  static const baseGrey10Color = Color(0xfff6f6f6);
+  static const baseWhiteColor = Color(0xffffffff);
+  static const basewhite60Color = Color(0xffF6F6F6);
+}
