15aecdccf44fb49d3d3d436122efa5387f68cb36
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-12-23 09:44:13 +0600

    1_main.dart

--- GetX_StateManagement/2_GetX_Named_Routing/1_main.dart
+++ GetX_StateManagement/2_GetX_Named_Routing/1_main.dart
@@ -1,1 +1,34 @@
-#
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: Text("Home Page"),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("You are now stay Home Page"),
+            ElevatedButton(
+              onPressed: () {
+                //Get.toNamed("/product");
+                //Get.offNamed('/product');
+                Get.offAllNamed('/product');
+
+              },
+              child: Text("Button-1"),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+            ),
+          ],
+        )));
+  }
+}
