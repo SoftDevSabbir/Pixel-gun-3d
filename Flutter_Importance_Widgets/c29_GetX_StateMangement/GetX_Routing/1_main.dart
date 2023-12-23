4b7684509eba9bae926a41afc7a43e427238958d
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2023-12-23 09:09:43 +0600

    main.dart

---
+++ Flutter_Importance_Widgets/c29_GetX_StateMangement/GetX_Routing/1_main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'HomePage.dart';
+
+void main(){runApp(MyApp());}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
