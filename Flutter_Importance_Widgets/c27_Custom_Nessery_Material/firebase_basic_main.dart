d62d4e88cd35fee27f3f1904f692ff26570dcb67
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2024-03-16 11:27:20 +0600

    firebase_basic_main.dart

---
+++ Flutter_Importance_Widgets/c27_Custom_Nessery_Material/firebase_basic_main.dart
@@ -0,0 +1,14 @@
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid? await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: 'AIzaSyDEMdpxJs6updjrSov2jHfsl9fSntA1Zcs',
+        appId: "1:857295007734:android:4ef7ba85b53ca745f861d7",
+        messagingSenderId: "857295007734",
+        projectId: "flutter-first-app-ea42f"
+    )
+  ):
+  await Firebase.initializeApp(
+  );
+  runApp(const MyApp());
+}
