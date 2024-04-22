74979c4ae30049a5ee3a390989d8bf2ce97bd074
MD SABBIR HOSSAIN <135597014+SoftDevSabbir@users.noreply.github.com>
2024-04-22 14:05:19 +0600

    Custom_TextFormField.dart

--- Custom_&_Reuseable_Widge/Custom_TextFormField.dart
+++ Custom_&_Reuseable_Widge/Custom_TextFormField.dart
@@ -1,1 +1,21 @@
-
+customTextFormField(TextEditingController controller, String text,
+      IconData iconData, bool toHide) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+      child: TextField(
+        controller: controller,
+        obscureText: toHide,
+        decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey, width: 3),
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueAccent, width: 3),
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            hintText: text,
+            suffixIcon: Icon(iconData),
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(15))),
+      ),
+    );
+  }
